060f0f715f65c4250b58596b3df8bee217d634b3
AMINE LEBRINI <aminelebrini4@gmail.com>
2026-09-02 11:53:13 +0100

    correction

--- python_challenges/challenge_7/main.py
+++ python_challenges/challenge_7/main.py
@@ -7,8 +7,8 @@ X2 = float(input("enter X2 : "))
 Y1 = float(input("enter Y1 : "))
 Y2 = float(input("enter Y2 : "))
 
-x_calcul = math.pow(X1 - X2, 2)
-y_calcul = math.pow(Y1 - Y2, 2)
+x_calcul = math.pow(X2 - X1, 2)
+y_calcul = math.pow(Y2 - Y1, 2)
 
 result = math.sqrt(x_calcul + y_calcul)
 
